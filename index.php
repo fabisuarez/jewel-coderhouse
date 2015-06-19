98549de0a9ec21c26cf03fc81955f325e653e637
Fabian Suarez <suarezfabian@outlook.com>
2015-06-19 18:17:44 -0300

    Maqueta y estructura de index.html nosotros.html productos.html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Jewel</title>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Hola</h1>
+
+
+</body>
+</html>
